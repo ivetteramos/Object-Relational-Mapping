8bc38a7c0117ca04303a8c116e4bf31bf16713e0
ivetteramos <101986174+ivetteramos@users.noreply.github.com>
2022-08-13 16:59:49 -0700

    added new code to the files

--- models/Product.js
+++ models/Product.js
@@ -7,16 +7,40 @@ const sequelize = require('../config/connection');
 class Product extends Model {}
 
 // set up fields and rules for Product model
-Product.init(
+ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references:
+       {
+        model: 'product',
+        key: 'id',
+    }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'tag',
+        key: 'id',
+    }
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: 'product_tag',
   }
 );
 
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,14 +7,43 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    },
+    product_name: {
+      type: DataTypes.STRING, 
+      allowNull: false,
+    
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: 'product',
   }
 );
+
 
 module.exports = ProductTag;
